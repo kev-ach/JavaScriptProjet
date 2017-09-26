ff8cdc759de296b781236bd027801595dd74073b
Kevin Achiche <achiche.kevin@hotmail.fr>
2017-09-26 10:37:52 +0200

    Ajout de Bootstrap et fontawesome

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <html>
 
   <head>
-    <title>Puissance 4</title>
+    <title>PUISSANCE 4 ACHICHE KEVIN</title>
 		<meta charset="utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
 		<link href="jeuP4.css" rel="stylesheet"/>
-    <script src="baseJS.js" rel="stylesheet"></script>
+
   </head>
 
   <body>
 
-
+    <div align="center">
+      <h1><span class="label label-danger">PUISSANCE 4 ACHICHE KEVIN</span></h1>
+      <button type="button">PLAY<i class="icon icon-arrow-circle-o-right" style="font-size:60px;"></i></button>
+      <br/><br/><br/><br/><br/>
+    </div>
     <div id="jeu">
-    Veuillez activer javascript pour jouer.
+
     </div>
 
 
 
-
+    <script src="baseJS.js"></script>
   </body>
 </html>
