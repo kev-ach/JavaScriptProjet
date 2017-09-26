d0e84b19238cc74843416671e1d52cdf1ade7945
Kevin Achiche <achiche.kevin@hotmail.fr>
2017-09-26 09:38:06 +0200

    Puissance 4 CSS/HTML/JS

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+  <head>
+    <title>Puissance 4</title>
+		<meta charset="utf-8">
+		<link href="jeuP4.css" rel="stylesheet"/>
+    <script src="baseJS.js" rel="stylesheet"></script>
+  </head>
+
+  <body>
+
+
+    <div id="jeu">
+    Veuillez activer javascript pour jouer.
+    </div>
+
+
+
+
+  </body>
+</html>
